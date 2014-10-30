61db12ae70d78f6ce860e474096ab093130e82a8
Thomas A Caswell <tcaswell@bnl.gov>
2014-10-30 18:32:13 -0400

    WIP : improved demos a bit

--- client/analysis_client.py
+++ client/analysis_client.py
@@ -6,21 +6,23 @@ import broker.config as cfg
 import time
 import select
 
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 q = Queue()
-s.connect((cfg.SEND_HOST, cfg.SEND_PORT))
-print 'Sockets now connected'
 
-s.setblocking(0)
-data = None
-ready = select.select([s], [], [], 10)
-if ready[0]:
-    try:
-        data = s.recv(4096)
-    except:
-        raise
-if data:
-    print 'data arrived at the client server', data
-    q.put(data)
-s.close()
+while True:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    s.connect((cfg.SEND_HOST, cfg.SEND_PORT))
+    print 'Sockets now connected'
+
+    s.setblocking(0)
+    data = None
+    ready = select.select([s], [], [], 10)
+    if ready[0]:
+        try:
+            data = s.recv(4096)
+        except:
+            raise
+    if data:
+        print 'data arrived at the client server', data
+        q.put(data)
+    s.close()
--- client/collection_client.py
+++ client/collection_client.py
@@ -2,19 +2,19 @@ import socket
 import random
 import broker.config as cfg
 import json
+import numpy as np
+import time
 
-range_val = random.randint(0,100)
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+for j in range(15):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+    s.connect((cfg.HOST, cfg.PORT))
+    a = [{'arman': 2, 'diego': 3, 'count': j},
+         {'arman': 2, 'img': np.random.rand(5, 5).tolist(), 'count': j},
+         ]
+    data = json.dumps(a)
 
-s.connect((cfg.HOST, cfg.PORT))
+    s.sendall(data)
 
-
-a = [{'arman': 2, 'diego': 3},{'arman': 2, 'diego': 3}, {'bruno': 25, 'stuart': 29}]
-data = json.dumps(a)
-
-s.sendall(data)
-
-s.close()
-
-
+    s.close()
+    time.sleep(1)
