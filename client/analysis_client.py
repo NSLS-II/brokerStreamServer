b21d2fa6ab3775576dba6882eac66777fa054133
Thomas A Caswell <tcaswell@gmail.com>
2014-10-30 23:28:55 -0400

    ENH : bunch of fixes
     - deal with data bigger than the buffer
     - yield more interesting data

--- client/analysis_client.py
+++ client/analysis_client.py
@@ -9,20 +9,29 @@ import select
 q = Queue()
 
 while True:
+    print('Top of the loop')
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     s.connect((cfg.SEND_HOST, cfg.SEND_PORT))
     print 'Sockets now connected'
 
-    s.setblocking(0)
+    #    s.setblocking(True)
     data = None
     ready = select.select([s], [], [], 10)
     if ready[0]:
-        try:
+        print('ready')
+
+        accume_data = []
+        data = s.recv(4096)
+        while len(data):
+            accume_data.append(data)
             data = s.recv(4096)
-        except:
-            raise
+
+        data = ''.join(accume_data)
+
+
     if data:
-        print 'data arrived at the client server', data
+        print 'data arrived at the client server', len(data)
         q.put(data)
     s.close()
+    time.sleep(.01)
