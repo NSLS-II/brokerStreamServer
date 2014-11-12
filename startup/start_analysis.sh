c5988d0a2fac2c74f3207b61fd5e5f678ffe060c
Ken L <klauer@bnl.gov>
2014-11-12 13:06:44 -0500

    ENH: startup scripts for analysis/collection examples (temporary)

---
+++ startup/start_analysis.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python -c "from broker.client import analysis_client; analysis_client.main()"
---
+++ startup/start_collection.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python -c "from broker.client import collection_client; collection_client.main()"
